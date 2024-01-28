19ddf06c3e3f299ca1a4064f1b3c9b64aff6bfc6
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2024-01-28 16:52:22 +0100

    Added bias to noise

--- alpha_comp/compositors/Leaves/noise.py
+++ alpha_comp/compositors/Leaves/noise.py
@@ -5,17 +5,18 @@ import itertools
 
 
 class Noise(Compositor):
-    def __init__(self, gray=True):
+    def __init__(self, gray=True, bias=1):
         super().__init__()
         self.noise_map = None
         self.gray = gray
+        self.bias = bias
 
     def initialize(self, width, height, limit):
         super().initialize(width, height, limit)
         if not self.gray:
-            self.noise_map = np.floor(np.random.rand(width, height, 3) * limit)
+            self.noise_map = np.floor(np.random.rand(width, height, 3) ** self.bias * limit)
         else:
-            self.noise_map = np.floor(np.random.rand(width, height) * limit)
+            self.noise_map = np.floor(np.random.rand(width, height) ** self.bias * limit)
             self.noise_map = np.array([self.noise_map, self.noise_map, self.noise_map])
             self.noise_map = np.transpose(self.noise_map, (1, 2, 0))
 
