7134043989f7409331745ec4003e1a9a7c2e62b3
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2024-02-02 00:27:27 +0100

    Added parallelization to mass rep composition

--- alpha_comp/alpha_comp.py
+++ alpha_comp/alpha_comp.py
@@ -3,6 +3,7 @@ import os
 import random
 from alpha_comp.compositor import Compositor
 import numpy as np
+from joblib import Parallel, delayed
 
 
 class AlphaComp:
@@ -35,27 +36,9 @@ class AlphaComp:
 
     @staticmethod
     def mass_rep_composition(path, save_path, compositor: Compositor, order="linear", save_masks=None, repeats=1):
-        for repeat in range(repeats):
-            dir = os.listdir(path)
-            if order == "reverse":
-                dir.reverse()
-            elif order == "random":
-                random.shuffle(dir)
-            stack_img = None
+        def f(i):
+            img = AlphaComp.mass_image_compositor(path, compositor, order, save_masks)
+            Image.fromarray(np.uint8(img)).save(os.path.join(save_path, f"out_{i}.png"))
 
-            for i, file in enumerate(dir):
-                print(f"Compositing {i}")
-                img = Image.open(os.path.join(path, file))
-                img = np.array(img)
-                if stack_img is None:
-                    stack_img = np.zeros(img.shape)
-                    compositor.initialize(img.shape[0], img.shape[1], len(dir))
-                mask = compositor.composite(i, img / 255)
+        Parallel(n_jobs=-1)(delayed(f)(i) for i in range(repeats))
 
-                stack_img = stack_img + np.multiply(img, mask)
-
-                if save_masks is not None:
-                    mask = Image.fromarray(np.uint8(np.clip(mask*255, 0, 255)))
-                    mask.save(os.path.join(save_masks, f"mask_{i}.png"))
-
-            Image.fromarray(np.uint8(stack_img)).save(os.path.join(save_path, f"out_{repeat}.png"))
