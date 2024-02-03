e2183a18e5b03efb4a881bcb8331252e07afb950
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2024-02-03 23:42:13 +0100

    Improved radial computing time

--- alpha_comp/Geos.py
+++ alpha_comp/Geos.py
@@ -3,17 +3,18 @@ import itertools
 
 
 def radial(width, height, radius_low, radius_high):
-    arr = np.zeros((width, height, 3))
+    arr = np.ones((width, height))
 
     center_vector = np.array([width / 2, height / 2])
 
     pixel_vectors = np.array(
-        [[i, j] for i, j in itertools.product(range(width), range(height)) if i != 0 != j])
+        [[i, j] for i, j in itertools.product(range(width), range(height))])
 
     pixel_vectors_center = pixel_vectors - center_vector
     pixel_normals = np.linalg.norm(pixel_vectors_center, axis=1)
-    for normal, pixel_vector in zip(pixel_normals, pixel_vectors):
-        if radius_low < normal < radius_high:
-            arr[pixel_vector[0], pixel_vector[1], :] = 1
+    pixel_normals = np.reshape(pixel_normals, (width, height))
+    arr[pixel_normals > radius_high] = 0
+    arr[pixel_normals < radius_low] = 0
+    arr = np.array([arr, arr, arr]).transpose((1, 2, 0))
 
     return arr
