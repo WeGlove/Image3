ff7aabda0b9842bd3f468f41f6085be5bd7efcd5
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2024-02-03 23:46:52 +0100

    Improved hard on radial computation time

--- alpha_comp/Geos.py
+++ alpha_comp/Geos.py
@@ -6,15 +6,14 @@ def radial(width, height, radius_low, radius_high):
     arr = np.ones((width, height))
 
     center_vector = np.array([width / 2, height / 2])
-
-    pixel_vectors = np.array(
-        [[i, j] for i, j in itertools.product(range(width), range(height))])
+    pixel_vectors = np.mgrid[0:width, 0:height].transpose((1, 2, 0))
 
     pixel_vectors_center = pixel_vectors - center_vector
-    pixel_normals = np.linalg.norm(pixel_vectors_center, axis=1)
-    pixel_normals = np.reshape(pixel_normals, (width, height))
+    pixel_normals = np.linalg.norm(pixel_vectors_center, axis=-1)
+
     arr[pixel_normals > radius_high] = 0
     arr[pixel_normals < radius_low] = 0
+
     arr = np.array([arr, arr, arr]).transpose((1, 2, 0))
 
     return arr
